5d387fac7e61c82ec1eaaa4ad8b8ba3ad1df50a1
Annie <annieyx@uw.edu>
2020-05-28 21:54:07 -0700

    Added Leaderboard

--- 441project/src/Home/Home.js
+++ 441project/src/Home/Home.js
@@ -4,15 +4,16 @@ import {button} from 'react';
 import Container from 'react-bootstrap/Container'
 import "./Home.css"
 import logo from './DawgPack.png'
+import leader from './Leaderboard.PNG'
 
 export default class Home extends Component {
   render () {
     return (
           <div className="screen">
-            <div className="info">
-              <button onClick={() => history.push('/Login')}><div id="ques">?</div></button>
-            </div>
             <div className="logo">
+              <div className="info">
+                <button onClick={() => history.push('/Login')}><div id="ques">?</div></button>
+              </div>
             <img src={logo} alt="Dawg Pack Showdown" />
             </div>
             <div className="nav">
@@ -26,15 +27,8 @@ export default class Home extends Component {
             </div>
               <div className="container">
                 <h2>Week 4 - Which team in each face off can do the most pushups in <u>1 minute</u></h2><br/>
-                <br/>
-                <div className="leader">
-                <h2><b>Top 5 Leaderboard</b></h2>
-                <p>1. M. Baseball</p>
-                <p>2. W. Golf</p>
-                <p>3. W. Soccer</p>
-                <p>4. M. Basketball</p>
-                <p>5. M. Rowing</p>
-                </div>
+                <h2><b>Current Leaderboard</b></h2>
+                  <img src={leader} alt="Leaderboard" />
                 <br/>
               </div>
           </div>
