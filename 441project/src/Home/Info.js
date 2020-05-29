4e2053ab143197c57a64029efb47cc4265f2af21
Annie <annieyx@uw.edu>
2020-05-29 09:15:22 -0700

    Finished info page

--- 441project/src/Home/Info.js
+++ 441project/src/Home/Info.js
@@ -9,7 +9,7 @@ import home from "../Home.PNG";
 export default class Info extends Component {
   render () {
     return (
-      <div>
+      <div className="infoPage">
         <div className="home">
           <Link to={{
             pathname: '/'
@@ -19,6 +19,41 @@ export default class Info extends Component {
         </div>
         <div className="top">
           <h1>WHAT IS DAWG PACK SHOWDOWN?</h1>
+        </div>
+        <br/>
+        <div>
+          <p>Dawg Pack Showdown is a 10 week fitness based competition where UW athletic teams face off against one another to earn points and climb up the leaderboard!</p>
+          <p>Your job is to support your favorite teams to do their best!</p><br/>
+          <p><b>WHAT CAN YOU DO?</b></p>
+          <div className="categories">
+            <div className="boxes">
+              <h3>Make your Predictions</h3>
+              <p>Predict which teams will succeed in their face off challenge of the week!</p><p>These predictions will then be sent to your UW email for safe keeping.</p><p>Come back next week to compare your predictions with the results (in View Results)</p><p>Get all your friends involved! (Note: restricted to UW students only)</p>
+              <Link to={{
+                pathname: '/Login'
+              }}>
+                <Button>START</Button>
+              </Link>
+            </div>
+            <div className="boxes">
+              <h3>Send a Cheer</h3>
+              <p>Have a favorite team you want to support in the challenge?</p><p>Send them a fun photo of yourself (and friends) to boost their motivation!</p><p>Your cheer is then displayed along others on the external displays of the booth.</p><p>Go check out the awesome cheers by your fellow huskies!</p>
+              <Link to={{
+                pathname: '/SendCheer'
+              }}>
+                <Button>START</Button>
+              </Link>
+            </div>
+            <div className="boxes">
+              <h3>View Results</h3>
+              <p>Want to know how your favorite teams are doing in their weekly challenges?</p><p>Check back every Monday at 8am to see updated the weekly challenge results.</p><p>Did you make your predictions last week? Check out who won!</p><p>Go see all the fun challenges that are coming up in the future!</p>
+              <Link to={{
+                pathname: '/Results'
+              }}>
+                <Button>START</Button>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     );
