c551344842c321cdf0388aa28f98bd67bdf0c241
Lucia Viña Patiño <lvina@denodo.com>
2020-06-02 10:35:02 -0700

    Edits

--- 441project/src/ExteriorDisplay/Slideshow.js
+++ 441project/src/ExteriorDisplay/Slideshow.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import PropTypes from "prop-types";
+import "shuffle-array";
 
 export default class Slideshow extends Component {
 
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    var shuffle = require('shuffle-array'),
+        collection = this.props.imageUrls;
+
     if (this.props.imageUrls.length < 1) return  <div></div>
 
     return (
         <Carousel autoPlay={true} showThumbs={false} infiniteLoop={true} interval={3500}>
-              {this.props.imageUrls.map((image) => (
+              {shuffle(collection).map((image) => (
                 <div>
                   <img src= {image} height='360' width='640'/>
                 </div>
