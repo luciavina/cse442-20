23a7861cfe3cc6242c8e3d149070beb4effa889b
Lucia Viña Patiño <lvina@denodo.com>
2020-06-01 17:00:30 -0700

    Edits

--- 441project/src/ExteriorDisplay/Slideshow.js
+++ 441project/src/ExteriorDisplay/Slideshow.js
@@ -6,13 +6,13 @@ import PropTypes from "prop-types";
 export default class Slideshow extends Component {
 
   render() {
-    if (this.props.imageUrls.length < 1) return  <div> Images not fetched! </div>
+    if (this.props.imageUrls.length < 1) return  <div></div>
 
     return (
         <Carousel autoPlay={true} showThumbs={false} infiniteLoop={true} interval={3500}>
               {this.props.imageUrls.map((image) => (
                 <div>
-                  return <img src= {image} height='360' width='640'/>
+                  <img src= {image} height='360' width='640'/>
                 </div>
             ))}
         </Carousel>
